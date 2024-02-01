a43369fa23c3b1b33ed7a81fe109dad66b1e5089
Ash CPC <jv.palha@gmail.com>
2024-02-01 17:52:26 -0300

    fix: Change from http to https

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "http://api.openweathermap.org",
+    baseURL: "https://api.openweathermap.org",
 })
 // /data/2.5/weather?q={city name}&appid={API key}
