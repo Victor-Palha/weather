a865c2659f4943f501719276be979c96a89fed6a
Ash CPC <jv.palha@gmail.com>
2024-02-01 17:38:50 -0300

    Fix: URL from react-router-dom

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,7 @@ export function Router(){
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home/>}/>
+                <Route path="/weather" element={<Home/>}/>
             </Routes>
         </BrowserRouter>
     )
